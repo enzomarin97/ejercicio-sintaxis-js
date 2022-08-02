ea0f847e205a5863810cbc85f35b3137dcf91967
Enzo Marin <enzzo.mariin@gmail.com>
2022-08-02 20:36:09 -0300

    ejercicio de sintaxis

---
+++ ejercicio2.js
@@ -0,0 +1,16 @@
+const libro = {
+    titulo:"js for dummies",
+    autor: "Chris Minnick ",
+    fecha: fecha_libro = new Date( 02-18-2022), 
+    URL: 'www.dummies.com/article/technology/programming-web-design/javascript/javascript-for-dummies-cheat-sheet-207619/'
+    
+}
+const lista = [
+    "enzo", 
+    24, 
+    false, 
+    fecha_nacimiento = new Date("september 28 1997"), 
+    libro 
+]
+
+console.log(lista)
